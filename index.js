49ca0eccd3a79e2d79e70e7602023161cf33b865
Djuslun <djuslun@yandex.ru>
2023-12-24 18:43:20 +0300

    fix cors client url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_URL }))
 app.use('/', router)
 
 const start = async () => {
